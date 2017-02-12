7632834f9da4b26f1541868c05a2e86e242ac47d
Óskar <Notandi@users.noreply.github.com>
2017-02-12 19:27:20 +0000

    sma breyting

--- Client/src/config/routes.js
+++ Client/src/config/routes.js
@@ -5,12 +5,14 @@ var Route = ReactRouter.Route;
 var hashHistory = ReactRouter.hashHistory;
 var IndexRoute = ReactRouter.IndexRoute;
 var Main = require('../components/Main');
+var MusicContainer = require('../containers/MusicContainer');
 
 
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
+      <IndexRoute component={MusicContainer} />
     </Route>
   </Router>
 );
